a5176221f9ca13be0a41a3027e235a41af385ad8
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-07 05:02:31 +0000

    More tests, more documentation

--- protoc-bin-vendored/README.md
+++ protoc-bin-vendored/README.md
@@ -12,4 +12,10 @@ protoc_bin_vendored::protoc_bin_path().unwrap()
 
 returns a path to a `protoc` binary packaged into the crate.
 
+Crate also packs `.proto` files distributed with protobuf:
+
+```rust
+protoc_bin_vendored::include_path().unwrap()
+```
+
 <!-- cargo-sync-readme end -->
--- protoc-bin-vendored/src/lib.rs
+++ protoc-bin-vendored/src/lib.rs
@@ -11,6 +11,14 @@
 //! ```
 //!
 //! returns a path to a `protoc` binary packaged into the crate.
+//!
+//! Crate also packs `.proto` files distributed with protobuf:
+//!
+//! ```no_run
+//! # let _ =
+//! protoc_bin_vendored::include_path().unwrap()
+//! # ;
+//! ```
 
 #![deny(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
@@ -83,21 +91,34 @@ pub fn protoc_bin_path() -> Result<PathBuf, Error> {
     })
 }
 
-pub fn include_path() -> Result<PathBuf, Error> {
-    Ok(match ArchCrate::detect()? {
+pub(crate) fn include_path_for_arch(arch_crate: &ArchCrate) -> PathBuf {
+    match arch_crate {
         ArchCrate::Linux_X86_32 => protoc_bin_vendored_linux_x86_32::include_path(),
         ArchCrate::Linux_X86_64 => protoc_bin_vendored_linux_x86_64::include_path(),
         ArchCrate::Linux_Aarch_64 => protoc_bin_vendored_linux_aarch_64::include_path(),
         ArchCrate::Linux_Ppcle_64 => protoc_bin_vendored_linux_ppcle_64::include_path(),
         ArchCrate::Macos_x86_64 => protoc_bin_vendored_macos_x86_64::include_path(),
         ArchCrate::Win32 => protoc_bin_vendored_win32::include_path(),
-    })
+    }
+}
+
+/// Include path which contains protobuf bundled `.proto` (like `descriptor.proto`).
+///
+/// Include directory content is guaranteed to be identical regardless of the platform.
+pub fn include_path() -> Result<PathBuf, Error> {
+    Ok(include_path_for_arch(&ArchCrate::detect()?))
 }
 
 #[cfg(test)]
 mod test {
+    use std::fs;
     use std::io::Read;
+    use std::path::Path;
+    use std::path::PathBuf;
     use std::process;
+
+    use crate::include_path_for_arch;
+    use crate::ArchCrate;
 
     #[test]
     fn include_path() {
@@ -105,6 +126,67 @@ mod test {
             .unwrap()
             .join("google/protobuf/descriptor.proto")
             .exists());
+    }
+
+    #[test]
+    fn arch_crates_includes_identical() {
+        fn compare_recursively(a: &Path, b: &Path) {
+            assert_eq!(a.is_file(), b.is_file());
+            if a.is_file() {
+                let a_content = fs::read(a).unwrap();
+                let b_content = fs::read(b).unwrap();
+                assert_eq!(a_content, b_content);
+            } else {
+                let mut a_files: Vec<PathBuf> = fs::read_dir(a)
+                    .unwrap()
+                    .map(|e| e.unwrap().path())
+                    .collect();
+                let mut b_files: Vec<PathBuf> = fs::read_dir(b)
+                    .unwrap()
+                    .map(|e| e.unwrap().path())
+                    .collect();
+                a_files.sort();
+                b_files.sort();
+                let mut a_files = a_files.as_slice();
+                let mut b_files = b_files.as_slice();
+                while !a_files.is_empty() || !b_files.is_empty() {
+                    let (a_next, a_rem) = a_files.split_first().unwrap();
+                    let (b_next, b_rem) = b_files.split_first().unwrap();
+
+                    compare_recursively(a_next, b_next);
+
+                    a_files = a_rem;
+                    b_files = b_rem;
+                }
+                assert!(a_files.is_empty());
+                assert!(b_files.is_empty());
+            }
+        }
+
+        compare_recursively(
+            &include_path_for_arch(&ArchCrate::Linux_X86_64),
+            &include_path_for_arch(&ArchCrate::Linux_X86_64),
+        );
+        compare_recursively(
+            &include_path_for_arch(&ArchCrate::Linux_X86_64),
+            &include_path_for_arch(&ArchCrate::Linux_X86_32),
+        );
+        compare_recursively(
+            &include_path_for_arch(&ArchCrate::Linux_X86_64),
+            &include_path_for_arch(&ArchCrate::Linux_Aarch_64),
+        );
+        compare_recursively(
+            &include_path_for_arch(&ArchCrate::Linux_X86_64),
+            &include_path_for_arch(&ArchCrate::Linux_Ppcle_64),
+        );
+        compare_recursively(
+            &include_path_for_arch(&ArchCrate::Linux_X86_64),
+            &include_path_for_arch(&ArchCrate::Macos_x86_64),
+        );
+        compare_recursively(
+            &include_path_for_arch(&ArchCrate::Linux_X86_64),
+            &include_path_for_arch(&ArchCrate::Win32),
+        );
     }
 
     #[test]
