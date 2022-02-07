8b563b24b9a52fe3b7856c7204dfee7f87032530
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-07 02:33:50 +0000

    Move update-bin.sh to the repository root

--- update-bin.sh
+++ update-bin.sh
@@ -7,7 +7,7 @@ set -ex
 cd "$(dirname "$0")"
 
 tag_name=$(curl -s https://api.github.com/repos/protocolbuffers/protobuf/releases/latest | grep tag_name | cut -d '"' -f 4)
-echo "$tag_name" >version.txt
+echo "$tag_name" >protoc-bin-vendored/version.txt
 echo "updating protoc binaries to version $tag_name" >&2
 
 for arch in linux-aarch_64 linux-ppcle_64 linux-x86_32 linux-x86_64 osx-x86_64 win32; do
@@ -16,15 +16,18 @@ for arch in linux-aarch_64 linux-ppcle_64 linux-x86_32 linux-x86_64 osx-x86_64 w
     echo "downloading $url..." >&2
     curl -sL "$url" --output "${TMPFILE}.zip"
     if [[ $arch == "win32" ]]; then
-        unzip -p "${TMPFILE}.zip" bin/protoc.exe >"bin/protoc-${arch}.exe"
+        unzip -p "${TMPFILE}.zip" bin/protoc.exe >"protoc-bin-vendored/bin/protoc-${arch}.exe"
     else
-        unzip -p "${TMPFILE}.zip" bin/protoc >"bin/protoc-${arch}"
+        unzip -p "${TMPFILE}.zip" bin/protoc >"protoc-bin-vendored/bin/protoc-${arch}"
     fi
     if [[ $arch == "linux-x86_64" ]]; then
-        rm -rf include
-        unzip "${TMPFILE}.zip" "include/*" -d .
         # Check we are in correct directory
-        test -e ../protoc-bin-vendored/README.md
+        test -e protoc-bin-vendored/README.md
+        (
+            cd protoc-bin-vendored
+            rm -rf include
+            unzip "${TMPFILE}.zip" "include/*" -d .
+        )
     fi
     rm "${TMPFILE}.zip"
 done
